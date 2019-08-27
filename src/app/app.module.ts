7412dff92635eefd94e6de9a61e6877c62725fbb
unknown <Administrator@PC-D-669.Skycliffitdomain.com>
2019-08-27 14:51:31 +0530

    updatedtask

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { TaskdisplayComponent } from './taskdisplay/taskdisplay.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+TaskdisplayComponent
+],
+imports: [
+BrowserModule,FormsModule,NgbModule,NgbPaginationModule,NgbAlertModule,ReactiveFormsModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
